d828757f792e0b9c003456751002fb011ed057b5
David Poulík <poulikdavid97@gmail.com>
2019-02-09 22:25:32 +0100

    second commit

--- gs-relational-data-access-master/initial/src/main/java/hello/Application.java
+++ gs-relational-data-access-master/initial/src/main/java/hello/Application.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@SpringBootApplication
 public class Application implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -19,10 +20,31 @@ public class Application implements CommandLineRunner {
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
-
+    
+    @Autowired
+    JdbcTemplate jdbc;
+    
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
+		log.info("Create table");
+		
+		jdbc.execute("DROP table Customers if EXISTS");
+		jdbc.execute("CREATE TABLE Customers(" + "id serial, first_name VARCHAR(50), last_name VARCHAR(50))");
+		
+		List<Object[]> name_split = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+				.stream().map(name -> name.split(" ")).collect(Collectors.toList());
+		
+		name_split.forEach(name -> log.info(String.format("Insert as first name and last name %s %s", name[0], name[1])));
+		
+		
+		jdbc.batchUpdate("INSERT INTO Customers(first_name, last_name) VALUES(?,?)",name_split);
+	
+		jdbc.query("SELECT * from Customers", 
+				(rs, row_num) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")))
+				.forEach(customer -> log.info(customer.toString()));
+		
 	}
+	
 }
--- project1-maven/src/hello/HelloWorld.java
+++ project1-maven/src/hello/HelloWorld.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.joda.time.LocalTime;
+import org.joda.time.LocalDate;
 
 public class HelloWorld {
 	public static void main(String[] args)
@@ -8,9 +9,12 @@ public class HelloWorld {
 		Greeter gr = new Greeter();
 		LocalTime time = new LocalTime();
 		
-		System.out.println("Aktuální čas" + time);
+		LocalDate date = new LocalDate();
 		
-		System.out.println(gr.sayHello());
+		System.out.println("Aktuální čas: " + time);
+		System.out.printf("Aktuální datum: " + date.getDayOfMonth() +"."+date.getMonthOfYear() +"."+ date.getYear());
+		
+		System.out.println("\n" + gr.sayHello());
 	}
 
 }
